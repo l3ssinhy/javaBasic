0975daba2966d34dc96dbd50cb212c017dee975d
l3ssinhy <lucas.050409@gmail.com>
2026-02-05 06:17:58 -0300

    Cálculos simples com while

---
+++ src/aula4/While.java
@@ -0,0 +1,24 @@
+package aula4;
+
+public class While {
+    static void main() {
+
+//        int i = 10;
+//        while (i < 11){
+//            IO.println("O que vai aontecer? " + i);
+//            i++;
+//        }
+//    }
+//        int i = 0;
+//        while (i <= 10) {
+//            IO.println(i);
+//            i += 2;
+//        }
+          int i = 1;
+          while(i <= 10){
+              IO.println(i);
+              i += 2;
+          }
+    }
+
+}
